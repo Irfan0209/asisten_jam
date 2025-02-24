f95fbbd345057a94b969ef3b0d9bfc48f91007dc
Irfan0209 <irfanardiansyah0209@gmail.com>
2025-02-24 10:38:21 +0700

    program pertama

---
+++ asisten_jam.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#include <FluxGarage_RoboEyes.h>
+roboEyes roboEyes; // create eyes
+
+void setup() {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C or 0x3D
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Startup robo eyes
+  roboEyes.begin(SCREEN_WIDTH, SCREEN_HEIGHT, 100); // screen-width, screen-height, max framerate - 60-100fps are good for smooth animations
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
